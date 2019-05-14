ed4bcdd6551fa6129b3ce79919d543e14811e7a8
“tiandenghao <tiandh987@163.com>
2019-05-14 08:26:03 +0800

    评论模块完成

--- src/page/order-detail/index.js
+++ src/page/order-detail/index.js
@@ -36,6 +36,71 @@ var page = {
                 });
             }
         });
+        
+        // 确认收货
+        $(document).on('click', '.confirmReceipt', function() {
+            $(".close").click();
+            _order.confirmReceipt(_this.data.orderNumber, function(res){
+                _mm.successTips("完成收货，评价一下购买的商品吧！");
+            	_this.loadOrderDetail();
+            }, function(err){
+            	_mm.errorTips(err.msg);
+            });
+        });
+        
+        // 星级评价-差评
+        $(document).on('click', '.bad', function() {
+            var productId = $(this).attr("productId");
+            var badSpan = document.getElementById("bad-" + productId);
+            var midSpan = document.getElementById("mid-" + productId);
+            var greatSpan = document.getElementById("great-" + productId);
+            badSpan.className = "glyphicon glyphicon-star-empty active";
+            midSpan.className = "glyphicon glyphicon-star-empty";
+            greatSpan.className = "glyphicon glyphicon-star-empty";
+            $("#one-"+productId).click();
+        });
+        // 星级评价-中评
+        $(document).on('click', '.mid', function() {      
+            var productId = $(this).attr("productId");
+            var badSpan = document.getElementById("bad-" + productId);
+            var midSpan = document.getElementById("mid-" + productId);
+            var greatSpan = document.getElementById("great-" + productId);
+            badSpan.className = "glyphicon glyphicon-star-empty active";
+            midSpan.className = "glyphicon glyphicon-star-empty active";
+            greatSpan.className = "glyphicon glyphicon-star-empty";
+            $("#two-"+productId).click();
+        });
+        // 星级评价-好评
+        $(document).on('click', '.great', function() {
+            var productId = $(this).attr("productId");
+            var badSpan = document.getElementById("bad-" + productId);
+            var midSpan = document.getElementById("mid-" + productId);
+            var greatSpan = document.getElementById("great-" + productId);
+            badSpan.className = "glyphicon glyphicon-star-empty active";
+            midSpan.className = "glyphicon glyphicon-star-empty active";
+            greatSpan.className = "glyphicon glyphicon-star-empty active";
+            $("#three-"+productId).click();
+        });
+        // 评价提交
+        $(document).on('click', '.btn-publish', function() {
+            // 获取订单No
+            var orderNo= document.getElementById("orderNo").innerText;
+            //获取商品Id
+            var productId = $(this).attr("productId");
+            //获取评价等级 1：差评 2：中评 3：好评
+            var rank = $('input[name="appraise"]:checked').val(); 
+            //获取评价内容
+            var appraise = document.getElementById("appraise-text-" + productId).value;
+            console.log(orderNo,productId,rank,appraise);
+            //调用接口传入数据库
+            _order.publishAppraise(orderNo, productId, rank ,appraise, function(res){
+                $(".close").click();
+                _mm.successTips("评价完成");
+            	_this.loadOrderDetail();
+            }, function(err){
+            	_mm.errorTips(err.msg);
+            });
+        });
 	},
 
 	// 加载订单详细信息
@@ -48,6 +113,7 @@ var page = {
             // 数据的适配
             _this.dataFilter(res.data);
 			// 渲染页面
+            // console.log(res.data);
 			orderDetailHtml = _mm.renderHtml(templateOrderDetail, res.data);
 			$content.html(orderDetailHtml);
 		}, function(err) {
@@ -57,8 +123,10 @@ var page = {
     
     // 数据的适配
     dataFilter : function(data){
-        data.needPay      = data.status == 10;
-        data.isCancelable = data.status == 10;
+        data.needPay        = data.status == 10;//去支付
+        data.isCancelable   = data.status == 10;//取消订单
+        data.confirmReceipt = data.status == 40;//等待客户收货
+        data.isAppraise     = data.status == 50 | 60;//等待客户评价
     }
 };
 
